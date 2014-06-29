2850c351436adf861ddb2bfca81f3cc1d0c92d93
makingitbettergo <makingitbettergo@gmail.com>
2014-06-29 21:27:41 +1200

    Tried Koans Clojure. These are basic.

---
+++ HelloWorld/src/koans/04_sets.clj
@@ -0,0 +1,19 @@
+(ns koans.04-sets)
+
+  "You can create a set by converting another collection"
+  (= #{3} (set '(3)))
+  ;it should not be a matter to take it as a list or vector
+  "Counting them is like counting other collections"
+  (= 3 (count #{1 2 3}))
+
+  "Remember that a set is a *mathematical* set"
+  (= #{1 2 3 4 5} (set '(1 1 2 2 3 3 4 4 5 5)))
+
+  "You can ask clojure for the union of two sets"
+  (= #{1 2 3 4 5} (clojure.set/union #{1 2 3 4} #{2 3 5}))
+
+  "And also the intersection"
+  (= #{2 3} (clojure.set/intersection #{1 2 3 4} #{2 3 5}))
+
+  "But don't forget about the difference"
+  (= #{1 4} (clojure.set/difference #{1 2 3 4 5} #{2 3 5}))
