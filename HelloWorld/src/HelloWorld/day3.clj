6f6886385d2956ae1d5aac517dfed2c6e682a6af
makingitbettergo <makingitbettergo@gmail.com>
2014-07-06 21:44:02 +1200

    working on #134 for 4clojure question. no solution yet.

---
+++ HelloWorld/src/HelloWorld/day3.clj
@@ -0,0 +1,17 @@
+(ns HelloWorld.day3)
+
+(= nil nil)
+(not= nil  get :a {:a nil :b 2})
+;(true? (get :a {:a nil :b 2}))
+
+((fn [x col] (
+               some #{x} col))
+;               contains? col x))
+  :s  {:s "a", :d "d"})
+
+
+;(fn [target col]
+  (for [i {:s "a", :d "d"}]
+           (if(= i :s)
+                  true))   
+  
