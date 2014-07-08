3d41bb40b6a16d33be0681c496115f4f40f39a3b
makingitbettergo <makingitbettergo@gmail.com>
2014-07-08 20:48:06 +1200

    trying 4clojure questions.

---
+++ HelloWorld/src/practice/untitled.clj
@@ -0,0 +1,35 @@
+(ns practice.untitled)
+
+(str "hi," " I'm" " Yue")
+(true? (fn [element coll]
+         (and
+          (contains? coll element)
+          (nil? (get (coll element)))))
+       :a {:a nil})
+
+
+(ns practice.untitled)
+
+(str "hi," " I'm" " Yue")
+
+(true? ((fn [element coll]
+  (and (contains? coll element)
+       (nil? (get coll element)))) 
+        :a {:a nil :b 1}))
+
+(true?  (#(and (contains? %2 %1)
+               (nil? (%2 %1)) )
+          :a {:a nil :b 2}))
+
+({:a "X"} :b)
+({:a "X"} :a)
+
+(last (take 2 {:a 1 :c 3 :d 5}))
+
+(last (take 2 '(1 3 5)))
+
+(fn f [p n]
+  (if (= n 0)
+    (first p)
+    (f (rest p) (- n 1))))
+;this is a recursion solution...
