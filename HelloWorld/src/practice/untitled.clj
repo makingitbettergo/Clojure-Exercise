2e74d2b205946bc614190ef009e1e8b061f17d81
makingitbettergo <makingitbettergo@gmail.com>
2014-07-13 13:20:55 +1200

    set functions

--- HelloWorld/src/practice/untitled.clj
+++ HelloWorld/src/practice/untitled.clj
@@ -38,3 +38,13 @@
 ((fn [v m]
    (zipmap m (repeat v)))
    0 [:a :b :c])
+
+;sets
+(clojure.set/superset? #{1 2} #{2})
+
+(clojure.set/subset? #{1} #{1 2})
+
+(clojure.set/superset? #{1 2} #{1 2})
+
+(clojure.set/subset? #{1 2} #{1 2})
+
